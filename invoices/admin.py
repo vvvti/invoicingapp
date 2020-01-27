a171478f413616e04e2de32c37d8d5c405a2e6e9
Patryk <patryk.kontewicz@poczta.fm>
2020-01-27 23:11:19 +0100

    prepare drf auth api

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . models import Invoice, Contractor
 
-# Register your models here.
+admin.site.register(Invoice)
+admin.site.register(Contractor)
--- invoicingapp/urls.py
+++ invoicingapp/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token', obtain_auth_token, name='obtain_token'),
 ]
 
 if settings.DEBUG:
