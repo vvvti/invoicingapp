86420e8b5ef0319ebb83d84ed1953cc905a2b61f
Patryk <patryk.kontewicz@poczta.fm>
2020-02-05 21:50:50 +0100

    add frontend components added invoices, form for invoices and dashboard components in fronted app

---
+++ frontend/src/components/invoices/Invoices.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Invoices extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Invoices List</h1>
+      </div>
+    );
+  }
+}
+
+export default Invoices;
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -5,7 +5,7 @@ export class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Fakturka
+          Fakturka app
         </a>
         <button
           className="navbar-toggler"
