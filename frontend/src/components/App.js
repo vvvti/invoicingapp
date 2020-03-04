77c02b00cf460f4e9f69a0a0582be1ef8d457a75
Patryk <patryk.kontewicz@poczta.fm>
2020-03-04 22:22:36 +0100

    reorganize frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,12 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-import Header from "./layout/Header";
-import Dashboard from "./invoices/Dashboard";
-
-import { Provider } from "react-redux";
-import store from "../store";
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
-      </Provider>
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
     );
   }
 }
