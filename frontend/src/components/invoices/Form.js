9253a443a11ec95c314dc57cfc88aea0bc4e1c10
Patryk <patryk.kontewicz@poczta.fm>
2020-03-10 20:44:49 +0100

    add coreUI and navbar

--- frontend/src/components/invoices/Form.js
+++ frontend/src/components/invoices/Form.js
@@ -5,6 +5,9 @@ export class Form extends Component {
     return (
       <div>
         <h1>Add invoice form</h1>
+        <button type="button" className="btn btn-outline-primary">
+          Primary Button
+        </button>
       </div>
     );
   }
