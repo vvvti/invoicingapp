6dade3fb42c0946b8295f88ca8090962ef8956e7
Patryk <patryk.kontewicz@poczta.fm>
2020-03-17 23:41:33 +0100

    create auth reducer and PrivateRoute

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -15,6 +15,7 @@ import Dashboard from "./invoices/Dashboard";
 import Alerts from "./layout/Alerts";
 import Login from "./accounts/Login";
 import Register from "./accounts/Register";
+import PrivateRoute from "./common/PrivateRoute";
 
 import "./App.css";
 
@@ -38,7 +39,7 @@ class App extends Component {
               <Alerts />
               <div className="container">
                 <Switch>
-                  <Route exact path="/" component={Dashboard} />
+                  <PrivateRoute exact path="/" component={Dashboard} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
                 </Switch>
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import invoices from "./invoices";
 import errors from "./errors";
 import messages from "./messages";
+import auth from "./auth";
 
 export default combineReducers({
   invoices,
   errors,
-  messages
+  messages,
+  auth
 });
