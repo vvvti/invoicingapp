479a25aaee998304e67577c73087c6a156ab0504
Patryk <patryk.kontewicz@poczta.fm>
2020-03-28 17:54:17 +0100

    create table in contractors component

--- frontend/src/components/invoices/Contractors.js
+++ frontend/src/components/invoices/Contractors.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getContractors } from "../../actions/contractors";
@@ -12,7 +12,39 @@ export class Contractors extends Component {
     this.props.getContractors();
   }
   render() {
-    return <div></div>;
+    return (
+      <Fragment>
+        <h2>Contractors</h2>
+        <table className="table table-stripes">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Contractor Name</th>
+              <th>NIP</th>
+              <th>REGON</th>
+              <th>Phone number</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.contractors.map(contractor => (
+              <tr key={contractor.id}>
+                <td>{contractor.id}</td>
+                <td>{contractor.contractor_name}</td>
+                <td>{contractor.contractor_nip}</td>
+                <td>{contractor.contractor_regon}</td>
+                <td>{contractor.contractor_phone}</td>
+                <td>{contractor.contractor_email}</td>
+                <td>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    );
   }
 }
 
